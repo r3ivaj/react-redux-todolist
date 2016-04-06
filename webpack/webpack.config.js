8466fd8b17256d9efcef6e60610bbd5c72c993bf
Javier Ponce <r3ivaj@Javiers-MacBook-Pro.local>
2016-04-06 14:21:26 -0500

    removed webpack-dev-server

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,24 +4,16 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/index',
   output: {
-    path: path.join(__dirname, 'public/dist'),
+    path: path.join(__dirname, '../public/dist'),
     filename: 'bundle.js',
     publicPath: '/dist/'
   },
-  devServer: {
-    hot: true,
-  },
   module: {
-    // preLoaders: [
-    //   {
-    //     test: /\.js$/,
-    //     loader: 'eslint',
-    //   }
-    // ],
     loaders: [
       {
         test: /\.js$/,
         loaders: ['babel'],
+        include: path.join(__dirname, '../src')
       }
     ]
   }
